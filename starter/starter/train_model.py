9c5d38c7c36d543b0861670dede306bc9ee96f89
vw0ugr9 <jan.weyers@cariad.technology>
2026-05-07 15:09:24 +0200

    implemented csv-data import

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,26 @@
 # Script to train machine learning model.
 
+from pathlib import Path
+import pandas as pd
+import yaml
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model
+
+# Configuration
+BASE_DIR = Path(__file__).resolve().parent
+CONFIG_PATH = BASE_DIR / "config.yaml"
+
+with open(CONFIG_PATH, "r") as f:
+    config = yaml.safe_load(f)
+
+data_path = (BASE_DIR / config["data_input_path"]).resolve()
 
 # Add code to load in the data.
+data = pd.read_csv(data_path)
+data.columns = (data.columns.str.strip())
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
