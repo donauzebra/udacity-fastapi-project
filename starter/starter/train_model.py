016bab95d10db150936b3f6735ba285c16230396
vw0ugr9 <jan.weyers@cariad.technology>
2026-05-07 15:40:20 +0200

    implemented model training

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import pandas as pd
+import pickle
 import yaml
 from sklearn.model_selection import train_test_split
 
@@ -17,6 +18,7 @@ with open(CONFIG_PATH, "r") as f:
     config = yaml.safe_load(f)
 
 data_path = (BASE_DIR / config["data_input_path"]).resolve()
+model_path = (BASE_DIR / config["model_output_path"]).resolve()
 
 # Add code to load in the data.
 data = pd.read_csv(data_path)
@@ -40,5 +42,11 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder_test, lb_test = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+with open(model_path, "wb") as file:
+    pickle.dump(model, file)
