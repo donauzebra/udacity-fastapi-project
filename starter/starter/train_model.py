b22d38a99de7738f9325f2a08a7abb856782d989
vw0ugr9 <jan.weyers@cariad.technology>
2026-05-08 11:00:58 +0200

    pylint improvements

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,8 +1,8 @@
-# Script to train machine learning model.
+"""Train and evaluate the census salary model, then stores the trained model."""
 
 from pathlib import Path
-import pandas as pd
 import pickle
+import pandas as pd
 import yaml
 from sklearn.model_selection import train_test_split
 
@@ -14,7 +14,7 @@ from ml.model import train_model, inference, compute_model_metrics
 BASE_DIR = Path(__file__).resolve().parent
 CONFIG_PATH = BASE_DIR / "config.yaml"
 
-with open(CONFIG_PATH, "r") as f:
+with open(CONFIG_PATH, "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 data_path = (BASE_DIR / config["data_input_path"]).resolve()
@@ -22,7 +22,7 @@ model_path = (BASE_DIR / config["model_output_path"]).resolve()
 
 # Add code to load in the data.
 data = pd.read_csv(data_path)
-data.columns = (data.columns.str.strip())
+data.columns = data.columns.str.strip()
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 if config["validation_method"] == "train_test":
